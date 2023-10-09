47b0c32aae2268d01ea110c9df8a7beaf746e6c5
uwasylvie <u.sylvie@outlook.com>
2023-10-09 16:58:05 +0200

    update new.jsx and messages.jsx files

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,35 +1,10 @@
 import React from 'react'
-import { useEffect } from "react";
-import { useState } from "react";
+
 import "./message.css"
-import axios from "axios";
 
 
 const Messages = () => {
-    const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    const fetchAllMessages = async () => {
-      try {
-        const res = await axios.get("/messages");
-        setMessages(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchAllMessages();
-  }, []);
-
-  console.log(messages);
-
-  const handleDelete = async (id) => {
-    try {
-      await axios.delete(`/messages/${id}`);
-      window.location.reload()
-    } catch (err) {
-      console.log(err);
-    }
-  };
+    
 
   
   return (
@@ -37,15 +12,15 @@ const Messages = () => {
       <h2>My Messages</h2> 
     <div className="messages">
     
-      {messages.map((message) => (
-        <div key={message.id} className="message">
+     
+        <div  className="message">
           
-          <h3>{message.title}</h3>
-          <p>{message.description}</p>
+          <h3></h3>
+          <p></p>
          
-          <button className="delete" onClick={() => handleDelete(message.id)}>Delete</button>
+          <button className="delete" >Delete</button>
         </div>
-      ))}
+      
     </div>
     
     </div>
